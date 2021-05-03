73a1c6aaf0d44326a07f048fdda1bc1ce87c6fd5
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-05-02 21:04:09 -0700

    add connection session

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 
 const app = express();
 const PORT  = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -28,8 +28,8 @@ app.use(session(sess));
 
 
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 
 app.use(express.json());
@@ -38,9 +38,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // turn on routes
-app.use(routes);
+// app.use(routes);
 
 // turn on connection to db and server
-sequelize.sycn({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
