9f567512743bba135bae787a7d12ba9380354aa5
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-05-04 21:21:10 -0700

    create find all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exlude: ['password'] }
+    })
+    .then(userData => res.json(userData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
