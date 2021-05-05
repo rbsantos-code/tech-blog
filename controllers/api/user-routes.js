87ab2475fdc6926a8aedb4456e71d47ce267f9da
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-05-04 21:30:30 -0700

    create find user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,11 +1,49 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment  } = require('../../models');
 
+
+// GET all users
 router.get('/', (req, res) => {
     User.findAll({
         attributes: { exlude: ['password'] }
     })
     .then(userData => res.json(userData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// GET single user
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exlude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'body', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(userData => {
+        if (!userData) {
+            res.status(404).json({ message: 'no user found with this id' });
+            return;
+        }
+        res.json(userData);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
